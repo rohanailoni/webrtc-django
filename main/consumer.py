393cca8d005fd5c0ca080ea83fa241f8e7f23bd0
rohanailoni <rohanailoni@gmail.com>
2021-06-24 16:47:08 +0400

    first comit

--- main/consumer.py
+++ main/consumer.py
@@ -54,4 +54,53 @@ class chat_fun(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps(message))
 
 class Audio(AsyncWebsocketConsumer):
-    pass
+    async def connect(self):
+        self.group_name="jatiratnalu"
+
+        await self.channel_layer.group_add(self.group_name,self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+       await self.channel_layer.group_discard(
+           self.group_name,
+           self.channel_name
+       )
+       print('hey user you are disconnected')
+
+
+
+    async def receive(self, text_data):
+
+        receive_data=json.loads(text_data)
+        message=receive_data['message']
+        action=receive_data['action']
+        if (action=="new-offer") or (action=="new-answer"):
+            reciver_channel_name= receive_data['message']['reciver_channel_name']
+            receive_data['message']['reciver_channel_name']=self.channel_name
+            await self.channel_layer.send(
+                reciver_channel_name
+                 ,
+                {
+                    'type': 'send_sdp',
+                    'receive_data': receive_data,
+                }
+
+            )
+            return
+
+        receive_data['message']['reciver_channel_name']=self.channel_name
+
+
+
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                'type':'send.sdp',
+                'receive_data':receive_data,
+            }
+
+        )
+    async  def send_sdp(self,event):
+        message=event['receive_data']
+        await self.send(text_data=json.dumps(message))
